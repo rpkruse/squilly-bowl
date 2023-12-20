dcdce903e8c61be8822c2af1ff5ea0ace26fdffe
Ryan Kruse <rpkruse1@gmail.com>
2023-12-20 13:10:19 -0600

    Added neat animation to home page

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,14 +1,35 @@
 import { Component } from '@angular/core';
 import { URL_ROUTES } from '../../shared/constants';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+  getSpanStyle(index: number): string {
+    return `--i:${index}`;
+  }
+
   get rulesRoute(): string { return URL_ROUTES.RULES }
   get createRoute(): string { return URL_ROUTES.CREATE }
+
+  get textSpans(): string[] {
+    const finalProduct: string[] = [];
+    const text = 'WELCOME TO THE SQUILLY BOWL!';
+
+    text.split('').forEach((letter: string) => {
+      if (letter.trim() === '') {
+        letter = '&nbsp;';
+      }
+      finalProduct.push(letter);
+    });
+
+    console.log(finalProduct);
+
+    return finalProduct;
+  }
 }
