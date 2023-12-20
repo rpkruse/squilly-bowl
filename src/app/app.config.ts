a731d0a52f77c16f673b08230108c187148aaa1d
Ryan Kruse <rpkruse1@gmail.com>
2023-12-20 09:16:26 -0600

    Fixed everything

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,9 @@
 import { ApplicationConfig } from '@angular/core';
-import { provideRouter, withHashLocation } from '@angular/router';
+import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { HashLocationStrategy } from '@angular/common';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes, withHashLocation())]
+  providers: [provideRouter(routes)]
 };
